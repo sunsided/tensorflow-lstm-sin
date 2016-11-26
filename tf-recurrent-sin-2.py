1cbb4fcff6e105cb8471e6b6b445df32dfc19df1
Markus Mayer <widemeadows@gmail.com>
2016-11-26 18:06:51 +0100

    learning different frequencies

---
+++ tf-recurrent-sin-2.py
@@ -0,0 +1,125 @@
+'''
+A Recurrent Neural Network (LSTM) implementation example using TensorFlow library.
+Inspired by https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+from generate_sample import generate_sample
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import tensorflow as tf
+from tensorflow.python.ops import rnn, rnn_cell
+
+
+'''
+To classify images using a recurrent neural network, we consider every image
+row as a sequence of pixels. Because MNIST image shape is 28*28px, we will then
+handle 28 sequences of 28 steps for every sample.
+'''
+
+# Parameters
+learning_rate = 0.001
+training_iters = 300000
+batch_size = 50
+display_step = 100
+
+# Network Parameters
+n_input = 1  # input is sin(x)
+n_steps = 100  # timesteps
+n_hidden = 150  # hidden layer num of features
+n_outputs = 50  # output is sin(x+1)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_steps, n_input])
+y = tf.placeholder("float", [None, n_outputs])
+
+# Define weights
+weights = {
+    'out': tf.Variable(tf.random_normal([n_hidden, n_outputs]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([n_outputs]))
+}
+
+
+def RNN(x, weights, biases):
+
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, n_steps, n_input)
+    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+    # Permuting batch_size and n_steps
+    x = tf.transpose(x, [1, 0, 2])
+    # Reshaping to (n_steps*batch_size, n_input)
+    x = tf.reshape(x, [-1, n_input])
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    x = tf.split(0, n_steps, x)
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+
+    # Get lstm cell output
+    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+pred = RNN(x, weights, biases)
+
+# Define loss (Euclidean distance) and optimizer
+individual_losses = tf.reduce_sum(tf.square(tf.sub(pred, y)), reduction_indices=1)
+loss = tf.reduce_mean(individual_losses)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        _, batch_x, __, batch_y = generate_sample(f=None, t0=None, batch_size=batch_size, samples=n_steps, predict=n_outputs)
+
+        # Reshape data to get 28 seq of 28 elements
+        batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+        batch_y = batch_y.reshape((batch_size, n_outputs))
+
+        # Run optimization op (backprop)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+        if step % display_step == 0:
+            # Calculate batch loss
+            loss_value = sess.run(loss, feed_dict={x: batch_x, y: batch_y})
+            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss_value))
+        step += 1
+    print("Optimization Finished!")
+
+    # Test the prediction
+    n_tests = 3
+    for i in range(1, n_tests+1):
+        plt.subplot(n_tests, 1, i)
+        t, y, next_t, expected_y = generate_sample(f=i, t0=None, samples=n_steps, predict=n_outputs)
+
+        test_input = y.reshape((1, n_steps, n_input))
+        prediction = sess.run(pred, feed_dict={x: test_input})
+
+        # remove the batch size dimensions
+        t = t.squeeze()
+        y = y.squeeze()
+        next_t = next_t.squeeze()
+        prediction = prediction.squeeze()
+
+        plt.plot(t, y, color='black')
+        plt.plot(np.append(t[-1], next_t), np.append(y[-1], prediction), color='red')
+        plt.ylim([-1, 1])
+        plt.xlabel('time [t]')
+        plt.ylabel('signal')
+
+    plt.show()

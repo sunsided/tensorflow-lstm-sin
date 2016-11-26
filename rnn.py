0e0aab4e38637bb60e284d11a6a73e66cabe793a
Markus Mayer <widemeadows@gmail.com>
2016-11-26 18:44:35 +0100

    refactored RNN into separate module

---
+++ rnn.py
@@ -0,0 +1,30 @@
+"""
+A Recurrent Neural Network (LSTM) implementation example using TensorFlow library.
+Inspired by https://github.com/aymericdamien/TensorFlow-Examples/
+"""
+
+import tensorflow as tf
+from tensorflow.python.ops import rnn, rnn_cell
+
+
+def RNN(x, weights, biases, n_input, n_steps, n_hidden):
+
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, n_steps, n_input)
+    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+    # Permuting batch_size and n_steps
+    x = tf.transpose(x, [1, 0, 2])
+    # Reshaping to (n_steps*batch_size, n_input)
+    x = tf.reshape(x, [-1, n_input])
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    x = tf.split(0, n_steps, x)
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+
+    # Get lstm cell output
+    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
--- tf-recurrent-sin-3.py
+++ tf-recurrent-sin-3.py
@@ -1,19 +1,19 @@
-'''
+"""
 A Recurrent Neural Network (LSTM) implementation example using TensorFlow library.
 Inspired by https://github.com/aymericdamien/TensorFlow-Examples/
-'''
+"""
 
 from __future__ import print_function
 
 from generate_sample import generate_sample
+from rnn import RNN
 
 import numpy as np
 import matplotlib.pyplot as plt
+# noinspection PyUnresolvedReferences
 import seaborn as sns
 
-
 import tensorflow as tf
-from tensorflow.python.ops import rnn, rnn_cell
 
 
 '''
@@ -46,30 +46,7 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_outputs]))
 }
 
-
-def RNN(x, weights, biases):
-
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
-
-    # Permuting batch_size and n_steps
-    x = tf.transpose(x, [1, 0, 2])
-    # Reshaping to (n_steps*batch_size, n_input)
-    x = tf.reshape(x, [-1, n_input])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.split(0, n_steps, x)
-
-    # Define a lstm cell with tensorflow
-    lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-    # Get lstm cell output
-    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
-
-    # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
-
-pred = RNN(x, weights, biases)
+pred = RNN(x, weights, biases, n_input, n_steps, n_hidden)
 
 # Define loss (Euclidean distance) and optimizer
 individual_losses = tf.reduce_sum(tf.square(tf.sub(pred, y)), reduction_indices=1)
